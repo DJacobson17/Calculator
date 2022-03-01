c33cd7c0336d29b6fe14d28b188795fefad8bdd9
DJacobson17 <davidbjacobson@gmail.com>
2022-03-01 13:23:55 -0600

    added complete functionality

--- script.js
+++ script.js
@@ -21,3 +21,66 @@ function operate(o,x,y) {
         : (o == "/") ? divide(x,y)
         : "Not an operator."
 }
+operatorButton = document.querySelectorAll('.buttonOperator');
+numberButton = document.querySelectorAll('.buttonNumber');
+decimalButton = document.getElementById('.');
+
+numberButton.forEach(item => {
+    item.addEventListener('click', event => {
+        displayValue(item.id)
+    })
+})
+
+decimalButton.addEventListener('click', decimalAction);
+
+function decimalAction() {
+    return (value.includes('.')) ?  alert("You already have a decimal.") : displayValue('.')
+}
+
+let value = "";
+let x = 0;
+let o = "";
+let y = 0;
+const screen = document.getElementById('display')
+
+function displayValue(num) {
+    value += num;
+    screen.innerHTML = value;
+}
+operatorButton.forEach(item => {
+    item.addEventListener('click', event1 => {
+        x = value;
+        o = decideOperand(item.id);
+        clearValue();
+        console.log(o);
+        console.log(x);
+    })
+})
+
+function decideOperand (operator) {
+    return (operator == "add") ? "+"
+        : (operator == "subtract") ? "-"
+        : (operator == "multiply") ? "*"
+        : "/"
+}
+function clearValue() {
+    value = "";
+}
+
+clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', event2 => {
+    clearValue();
+    displayValue("");
+    x = 0;
+    y = 0;
+})
+equals = document.getElementById('equals');
+equals.addEventListener('click', event3 => {
+    y = value;
+    if ((o == "/") && (y == "0")) { 
+        alert("You dumb bastard! You can't divde by zero.");
+    } else {let answer = operate(o,Number(x),Number(y));
+        screen.innerHTML = +answer.toFixed(2);
+        value = answer;
+    }    
+})
